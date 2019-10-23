95b9ae7b97c740f5f8cedb4ad49486587fc2a1a9
aminamaludin <38432848+aminamaludin@users.noreply.github.com>
2019-10-23 19:30:54 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,28 @@
-<?php
-	/**
-	* File login.php
-	*
-	*/
+<?php 
 
-	/**
-	* Class login
-	* @package 		Login Form
-	* @author 		(Amin <aminamaludin001@gmail.com>)
-	* @version      v.0.0.1 (login form)
-	*/
+ /**
+  * File fungsi_db.php
+  *
+  */
 
-?>
+  /**
+  * Class Fungsi_db
+  * @package    Login
+  * @author     (Amin <aminamaludin001@gmail.com>)
+  * @version    v.0.0.1 (dalam pencarian error)
+  */
+
+ ?>
+
+
 <html>
     <head>
-       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
      
       <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-        <link rel="stylesheet" href="style.css" >
+      <link rel="stylesheet" href="style.css" >
      
     </head>
 <body>
@@ -37,19 +40,19 @@
                           <span class="card-title">Login</span>
                           <div class="input-field">
                               <input id="email" type="email" class="validate">
-                              <label for="email">Email</label>
+                              <label for="email">Username</label>
                           </div>
                           <div class="input-field" >
                               <input id="password" type="password" class="validate">
                               <label for="password">Password</label>
                             
                           </div>
-                          <button class="btn waves-effect waves-light" type="submit" name="action">Login
+                          <button class="btn waves-effect waves-light" type="submit" name="login">Login
                               <i class="material-icons right">send</i>
                           </button>
 
                       </div>
-                      <p>Don't have an account ? <a href="">Register Now !</a></p>
+                      <p>Belum punya akun ? <a href="register.php">Daftar sekarang !</a></p>
                       <br>
                      
                           
@@ -63,6 +66,6 @@
   </div>
 </div>
  
- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
